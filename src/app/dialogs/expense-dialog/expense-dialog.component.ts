cb6db425ecf53302967d999664dac7b8e6515097
Balaji Krishnan <balaji2k9_srec14@ymail.com>
2020-08-14 00:28:46 +0530

    updates

--- src/app/dialogs/expense-dialog/expense-dialog.component.ts
+++ src/app/dialogs/expense-dialog/expense-dialog.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { RestService } from 'src/app/rest.service';
+import { ExpenseModel } from 'src/app/models/expense.model';
+import { CategoryModel } from 'src/app/models/category.model';
+import { Asset } from 'src/app/models/asset.model';
 
 @Component({
   selector: 'app-expense-dialog',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpenseDialogComponent implements OnInit {
 
-  constructor() { }
+  exp: ExpenseModel;
+  categories: CategoryModel[] = [];
+  assets: Asset[] = [];
+  constructor(@Inject(MAT_DIALOG_DATA) public data,
+  private service: RestService,
+  private dialog: MatDialogRef<ExpenseDialogComponent>) {
+    this.exp = data.model ? data.model  : this.getDefaultExpenseModel();
+    this.categories = data.categories;
+    this.assets = data.assets;
+  }
 
   ngOnInit(): void {
+  }
+
+  cancel() {
+    this.dialog.close();
+  }
+
+  confirm() {
+    this.dialog.close({
+
+    });
+  }
+
+  getDefaultExpenseModel(): ExpenseModel {
+    return {
+      id: null,
+      amount: null,
+      asset: null,
+      comment: null,
+      category: null,
+      subCategory: null,
+      transactionDate: null
+    };
   }
 
 }
